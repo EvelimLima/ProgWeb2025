2c66bf3e7394459974534f05efbede81d022aaf8
EvelimLima <evelimllima@gmail.com>
2025-04-24 15:13:21 -0400

    #

--- Exercícios/JS2_jokenpô/Jokenpô.js
+++ Exercícios/JS2_jokenpô/Jokenpô.js
@@ -1,10 +1,10 @@
-let item = ["Papel", "Pedra", "Tesoura"];
+let item = ["Papel", "Pedra", "Tesoura"]
 let pontuacao = 0;
-let perdeu = false;
+let perdeu = false
 
 while (!perdeu) { 
-    console.log("Escolha sua jogada:");
-    console.log("1 - Papel\n2 - Pedra\n3 - Tesoura");
+    console.log("Escolha sua jogada:")
+    console.log("1 - Papel\n2 - Pedra\n3 - Tesoura")
 
     let escolhaUsuario = parseInt(prompt("Digite o número correspondente à sua jogada (1, 2 ou 3):"));
     if (isNaN(escolhaUsuario) || escolhaUsuario < 1 || escolhaUsuario > 3) { 
@@ -15,7 +15,7 @@ while (!perdeu) {
 
     let escolhaComputador = Math.floor(Math.random() * 3) + 1;
     console.log(`Você jogou ${item[escolhaUsuario - 1]}`);
-    console.log(`O computador jogou ${item[escolhaComputador - 1]}`);
+    console.log(`O computador jogou ${item[escolhaComputador - 1]}`)
 
     let diferencaEscolha = escolhaComputador - escolhaUsuario;
 
@@ -23,11 +23,11 @@ while (!perdeu) {
         console.log("Você ganhou esta rodada!");
         pontuacao++; //incrementa a pontuação
     } else if (escolhaUsuario === escolhaComputador) {
-        console.log("Esta rodada empatou!");
+        console.log("Esta rodada empatou!")
     } else {
-        console.log("Você perdeu esta rodada!");
+        console.log("Você perdeu esta rodada!")
         perdeu = true;
     }
 }
 
-console.log(`Jogo encerrado! Sua pontuação total foi de: ${pontuacao}.`);
+console.log(`Jogo encerrado! Sua pontuação total foi de: ${pontuacao}.`)
